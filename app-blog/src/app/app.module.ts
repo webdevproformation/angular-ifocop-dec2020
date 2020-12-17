fd3dff5dc9581bf5d2dfd38e5ad750cd42fd917c
malik <malik.h@webdevpro.net>
2020-12-17 09:28:40 +0100

    start jour 2

--- app-blog/src/app/app.module.ts
+++ app-blog/src/app/app.module.ts
@@ -2,29 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PremierComponent } from './premier.component';
-import { DeuxiemeComponent } from './deuxieme.component';
-import { ListeComponent } from './liste/liste.component';
-import { ArticleComponent } from './article/article.component';
-import { CategoriesService } from "./services/categories.service" ;
-import { DataService } from './services/data.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent ,
-    PremierComponent,
-    DeuxiemeComponent,
-    ListeComponent,
-    ArticleComponent
+    AppComponent 
   ],
   imports: [
-  
 BrowserModule
   ],
   providers: [
-    CategoriesService,
-    DataService
   ],
   bootstrap: [AppComponent]
 })
