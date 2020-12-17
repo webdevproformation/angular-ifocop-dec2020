09e037c7640910f559007a2058131c350ac4a21d
malik <malik.h@webdevpro.net>
2020-12-17 10:54:53 +0100

    correction

--- app-blog/src/app/app.module.ts
+++ app-blog/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent 
+    ArticleComponent,
+    MenuComponent 
   ],
   imports: [
 BrowserModule
