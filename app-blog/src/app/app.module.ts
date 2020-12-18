dc5f679fd8ac272eb98749d04a4ffb45c754e105
malik <malik.h@webdevpro.net>
2020-12-18 10:35:00 +0100

    formulaires

--- app-blog/src/app/app.module.ts
+++ app-blog/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    SearchFormComponent
   ],
   imports: [
 BrowserModule
