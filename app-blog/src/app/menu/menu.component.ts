4d75478557ff7e4250a1b9c8ab70625877505b22
malik <malik.h@webdevpro.net>
2020-12-18 10:09:37 +0100

    correction

---
+++ app-blog/src/app/menu/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-menu',
+  template: `
+    <div class="row">
+      <nav class="navbar navbar-expand bg-dark navbar-dark col-12">
+        <img [src]="url" alt="" class="navbar-brand img-thumbnail">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item" *ngFor="let m of menu">
+            <a href="" class="nav-link">
+              {{ m }}
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  `
+})
+export class MenuComponent {
+  url : string = "https://via.placeholder.com/60x40?text=jour3";
+  menu : Array<string> = ["Accueil","Articles", "Connexion"];
+}
