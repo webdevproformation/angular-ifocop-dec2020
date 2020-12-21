e07449ece23107e8dbaaaa443b8b2e3143277495
malik <malik.h@webdevpro.net>
2020-12-21 11:32:34 +0100

    fin formulaire de connexion

--- app-blog/src/app/connexion/connexion.component.ts
+++ app-blog/src/app/connexion/connexion.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnexionComponent implements OnInit {
 
+  compteUser = {
+    login : "login",
+    mdp : "azerty",
+    role : "admin"
+  }
+  show : boolean = false ;
+
+  onSubmit(f){
+    if(f.valid){
+      console.log(f.value)
+      if( f.value.login === this.compteUser.login && f.value.password === this.compteUser.mdp ){
+        // redirection vers la page d'accueil du back office du site 
+        // stocker dans le localStorage une information qui dit => c'est la administrateur 
+      } else {
+        f.reset();
+        this.show = true ;
+        setTimeout( () => { 
+          this.show = false ;
+        } , 2000)
+      }
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
