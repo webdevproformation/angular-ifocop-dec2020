92a5aa883b6e9dd03121b0376ab7e20fe78cecaa
malik <malik.h@webdevpro.net>
2020-12-22 12:33:43 +0100

    pade d'accueil admin site

--- app-blog/src/app/admin/home/home.component.ts
+++ app-blog/src/app/admin/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../../services/data.service";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  nom : string = "";
+  articles : Array<{ _id: string , company : string , about:string}> = []
 
-  constructor() { }
+  constructor( private _data : DataService) { }
 
   ngOnInit(): void {
+    this.articles = this._data.getAll();
+    this.nom = JSON.parse(localStorage.getItem("token")).nom ;
   }
 
 }
